c7cf626da2072836e051e34bbfcfde0976f1f8cb
Terrell Douglas <terrelloydouglas@yahoo.com>
2019-10-01 13:58:40 -0700

    question 2 finished

--- lab.js
+++ lab.js
@@ -3,7 +3,17 @@ var military = prompt('Was I in the military?');
 military = military.toLowerCase();
 
 if(military === 'yes') {
-    alert('Correct!');
+  alert('Correct!');
 } else {
-    alert('Wrong!');
+  alert('Wrong!');
+}
+
+var tattoo = prompt('Do I have tattoos?');
+
+tattoo = tattoo.toLowerCase();
+
+if(tattoo === 'yes') {
+  alert('Sure Do, two on my right arm.');
+} else {
+  alert('Wrong!!');
 }
