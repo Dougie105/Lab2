f2e1173205a891f915ba56182e4308dee103e8f3
Terrell Douglas <terrelloydouglas@yahoo.com>
2019-10-01 19:09:55 -0700

    fixing errors with questions 4 and 5 as well as adding console.log and commenting it out

--- lab.js
+++ lab.js
@@ -10,8 +10,10 @@ var military = prompt('Was I in the military?');
 military = military.toLowerCase();
 
 if(military === 'yes'|| military === 'y') {
+  // console.log('Correct!');
   alert('Correct!');
 } else {
+  // console.log('Wrong!');
   alert('Wrong!');
 }
 
@@ -22,8 +24,10 @@ var tattoo = prompt('Do I have tattoos?');
 tattoo = tattoo.toLowerCase();
 
 if(tattoo === 'yes' || tattoo === 'y') {
+  // console.log('Correct!');
   alert('Sure Do, two on my right arm.');
 } else {
+  // console.log('Wrong!');
   alert('Wrong!!');
 }
 
@@ -34,8 +38,10 @@ var piercing = prompt('Do I have any piercings?');
 piercing = piercing.toLowerCase();
 
 if(piercing === 'no' || piercing === 'n'){
+  // console.log('Correct!');
   alert('Correct, I dont have any');
 } else {
+  // console.log('Wrong!');
   alert('Wrong!!!');
 }
 
@@ -45,9 +51,11 @@ var married = prompt('Am I married?');
 
 married = married.toUpperCase();
 
-if(married === 'no' || married === 'n'){
+if(married === 'NO' || married === 'N'){
+  // console.log('Wrong!');
   alert('Wrong!!!!');
 } else {
+  // console.log('Correct!')
   alert('Correct! for 5 years!');
 }
 
@@ -57,10 +65,12 @@ var codeWork = prompt('Do I like coding');
 
 codeWork = codeWork.toUpperCase();
 
-if(codeWork === 'yes' || codeWork === 'y'){
-  alert('WRONG!!!!! I LOVE it');
+if(codeWork === 'YES' || codeWork === 'Y'){
+  // console.log('Correct!');
+  alert('Correct!!!!! I LOVE IT');
 } else {
-  alert('Correct! Because I love it!');
+  // console.log('Wrong!');
+  alert('Wrong!!!!!');
 }
 
 alert('Thank you for playing ' + aboutYou + '! Have a wonderful day!');
