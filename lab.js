1ba40b3d0686abab461c2597bbba7058bb37fa94
Terrell Douglas <terrelloydouglas@yahoo.com>
2019-10-02 20:15:53 -0700

    lab 3 done for now... its 815pm

--- lab.js
+++ lab.js
@@ -77,23 +77,53 @@ if(codeWork === 'YES' || codeWork === 'Y'){
 
 
 var answer = 7;
+//console.log('Guess a number 1-10, you have 4 attempts');
 alert('Guess a number 1-10, you have 4 attempts');
 
 for (var i=0; i<4; i++) {
+  //console.log('Pick a number!');
   var attempt = prompt('Pick a number!');
   attempt = parseInt(attempt);
 
   if (attempt === answer) {
+    //console.log('YAAAY!');
     alert('YAAAY!');
     break;
   }
   if (attempt > answer) {
+    //console.log('Too high, try again!');
     alert('Too high, try again!');
   }
   if (attempt < answer) {
+    //console.log('Too low, Try again!');
     alert('Too low, Try again!');
   }
   if (i === 3) {
+    //console.log('GAME OVER! The Number Is 7');
     alert('GAME OVER! The Number Is 7');
+  }
+}
+
+//Question 7
+
+var states = ['Indiana', 'Ohio', 'Florida', 'Virginia', 'Georgia'];
+
+//console.log('Guess a state that I have lived in! You get 6 attempts.');
+alert('Guess a state that I have lived in! You get 6 attempts.');
+
+for (var s=0; s<states.length+1 ; s++) {
+  //console.log('Try your luck');
+  var guess = prompt('Try your luck');
+
+  if (guess === states) {
+  //console.log('YAAAY! ' + 'Ive lived in ' + states);
+    alert('YAAAY! ' + 'Ive lived in ' + states);
+    break;
+  } else
+  //console.log('Try Again!')
+    alert('Try Again!');
+  if (s === 5) {
+  //console.log('GAME OVER! Ive lived in ' + states);
+    alert('GAME OVER! Ive lived in ' + states);
   }
 }
